3117143d79e588c0334e74121314e61eb02b70e5
Shanmugavel Sundaramoorthy <shanmugavel.rs@gmail.com>
2017-09-23 21:23:59 -0400

    feat: Add search functionality
    Add code required to invoke search api and show the results.

--- src/searchbooks.js
+++ src/searchbooks.js
@@ -1,9 +1,57 @@
 import React, {Component} from 'react';
+import DebounceInput from 'react-debounce-input';
+import * as BooksAPI from './BooksAPI';
+import Book from './book';
+
 
 class SearchBooks extends Component {
 
+    state = {
+        query : '',
+        books : []
+    }
+
+    clearBooks = () => {
+        this.setState({ books : [] });
+    }
+
+    updateQuery = (inputObj) => {
+        if(inputObj.query.trim().length > 0) {
+            this.setState({ query: inputObj.query.trim() });
+            BooksAPI.search(this.state.query, 20)
+               .then((books) => {
+                   this.setState({ books });
+               }).catch(err => {
+                   console.log('Error:' + err);
+                   this.clearBooks();
+               });
+        } else {
+            this.clearBooks();
+        }
+    }
+
     render () {
-        return <h1>Search TBD</h1>
+        return (
+            <div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" href="/">Close</a>
+              <div className="search-books-input-wrapper">
+                <DebounceInput
+                    minLength={3}
+                    debounceTimeout={200}
+                    placeholder="Search by title or author" 
+                    onChange={event => this.updateQuery({query: event.target.value})} />
+                </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid">
+                {
+                    this.state.books && this.state.books.length > 0 && this.state.books.map((book, index) => <Book key={index} book={book} />)
+                }
+              </ol>
+            </div>
+          </div>
+        );
     }
 
 }
